5ce97ef6c371ad5e339c6b6210e6ed28bdb62a22
21r8390 <21r8390@bztf.ch>
2022-10-26 00:10:12 +0200

    🦄: Twig Dropdown überarbeitet

--- docker/Aufgaben/006_MVC_TWIG/app/app/controllers/flugzeug.php
+++ docker/Aufgaben/006_MVC_TWIG/app/app/controllers/flugzeug.php
@@ -6,17 +6,20 @@ class Flugzeug extends Controller
 {
     public function index($name = '')
     {
+        // Flugzeug Model laden und befüllen
         $flugzeug = $this->model('Flugi');
         $flugzeug->id = 1;
         $flugzeug->name = "Boing 747";
         $flugzeug->speed = 1000;
         $flugzeug->typ = FlugiTypen::Ballon;
 
+        // Typen laden
         $typen = array_map(
             fn ($enumItem) => "{$enumItem->value}",
             FlugiTypen::cases()
         );
 
+        // View laden
         echo $this->twig->render('flugzeug/index.twig.html', ['title' => "Flugzeug / Index", 'flugzeug' => $flugzeug, 'typen' => $typen]);
     }
 }
